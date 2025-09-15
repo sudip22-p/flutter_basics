d4ebc0b01f00be97aaa1faee1baefed7815e4c10
Sudip Paudel <paudelsudip134@gmail.com>
2025-09-15 18:38:35 +0545

    fix: auth-bloc code optimization

--- login_handling_bloc/lib/bloc/auth_bloc.dart
+++ login_handling_bloc/lib/bloc/auth_bloc.dart
@@ -6,43 +6,52 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthLoginButtonPresssed>((event, emit) async {
-      try {
-        emit(AuthLoading());
-        //get the email password and check the validity
-        final email = event.email;
-        final password = event.password;
-        final RegExp emailRegex = RegExp(
-          r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
-        );
-        if (!emailRegex.hasMatch(email)) {
-          return emit(AuthFailure(errorMessage: "Invalid email address! "));
-        }
-        if (password.length < 6) {
-          return emit(
-            AuthFailure(
-              errorMessage: "Password must be at least 6 character long.",
-            ),
-          );
-        }
-
-        await Future.delayed(const Duration(seconds: 1), () {
-          //do db func then ...
-          return emit(AuthSuccess(uid: email));
-        });
-      } catch (e) {
-        return emit(AuthFailure(errorMessage: e.toString()));
-      }
-    });
-    on<AuthLogoutButtonPressed>((event, emit) async {
+    on<AuthLoginButtonPresssed>(_onLoginButtonPressed);
+    on<AuthLogoutButtonPressed>(_onLogoutButtonPressed);
+  }
+  void _onLoginButtonPressed(
+    AuthLoginButtonPresssed event,
+    Emitter<AuthState> emit,
+  ) async {
+    try {
       emit(AuthLoading());
-      try {
-        await Future.delayed(const Duration(seconds: 1), () {
-          return emit(AuthInitial());
-        });
-      } catch (e) {
-        emit(AuthFailure(errorMessage: e.toString()));
+      //get the email password and check the validity
+      final email = event.email;
+      final password = event.password;
+      final RegExp emailRegex = RegExp(
+        r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+      );
+      if (!emailRegex.hasMatch(email)) {
+        return emit(AuthFailure(errorMessage: "Invalid email address! "));
       }
-    });
+      if (password.length < 6) {
+        return emit(
+          AuthFailure(
+            errorMessage: "Password must be at least 6 character long.",
+          ),
+        );
+      }
+
+      await Future.delayed(const Duration(seconds: 1), () {
+        //do db func then ...
+        return emit(AuthSuccess(uid: email));
+      });
+    } catch (e) {
+      return emit(AuthFailure(errorMessage: e.toString()));
+    }
+  }
+
+  void _onLogoutButtonPressed(
+    AuthLogoutButtonPressed event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthInitial());
+      });
+    } catch (e) {
+      emit(AuthFailure(errorMessage: e.toString()));
+    }
   }
 }
